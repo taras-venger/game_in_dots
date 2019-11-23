6ea4948612eb6aca14b251d626763c7f8f144e78
Taras Venger <taras-venger@users.noreply.github.com>
2019-11-23 22:57:50 +0200

    10 latest winners included into response

--- routes/winners.js
+++ routes/winners.js
@@ -2,27 +2,27 @@ const express = require('express');
 const router = express.Router();
 const Winner = require('../models/Winner');
 
-// Get the list
-router.get('/', async (req, res) => {
+// Get records of 10 latest results
+const getWinners = async response => {
   try {
-    const winnersList = await Winner.find();
-    res.json(winnersList);
+    const winnersList = await Winner.find()
+      .sort({ _id: -1 })
+      .limit(10);
+    response.json(winnersList);
   } catch (err) {
-    res.status(500).json({ message: err.mesage });
+    response.json({ message: err.mesage });
   }
-});
+};
 
-// Report new winner
+router.get('/', async (req, res) => getWinners(res));
+
 router.post('/', async (req, res) => {
   const winner = new Winner({
-    name: req.body.name
+    name: req.body.name,
+    date: req.body.date
   });
-  try {
-    const newWinner = await winner.save();
-    res.json(newWinner);
-  } catch (err) {
-    res.json({ message: err.mesage });
-  }
+  await winner.save();
+  getWinners(res);
 });
 
 module.exports = router;
