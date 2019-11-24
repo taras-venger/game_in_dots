cff11c6c4bfc27458af99ed64053c5960be8649f
Taras Venger <taras-venger@users.noreply.github.com>
2019-11-25 01:22:23 +0200

    Added time, restyled with Semantic-UI

--- client/src/components/Winners.js
+++ client/src/components/Winners.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import '../App.css';
+import { Message } from 'semantic-ui-react';
+
+const timeToStr = minutes => (minutes < 10 ? `0${minutes}` : minutes);
 
 function Winners(props) {
   const { winnersList } = props;
 
   const renderWinners = winnersList.map(el => {
     const date = new Date(el.date);
-    const dmyDate = `${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`;
+    const dateTime = `${date.getDate()}.${date.getMonth()}.${date.getFullYear()} ${timeToStr(
+      date.getHours()
+    )}:${timeToStr(date.getMinutes())}`;
     return (
-      <li key={el._id} className='winner'>
-        {el.name}
-        {dmyDate}
-      </li>
+      <Message key={el._id} className='winner-message'>
+        <span>{el.name}</span>
+        <span>{dateTime}</span>
+      </Message>
     );
   });
 
   return (
     <div className='Winners'>
+      <div className='MessageBox'>Leaders board</div>
       <ul>{renderWinners}</ul>
     </div>
   );
